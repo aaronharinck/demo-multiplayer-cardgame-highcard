d079c9480f63298c1deddd6718115f55523ce3a3
aaronharinck <aaron.harinck@student.howest.be>
2020-11-26 08:21:43 +0100

    add nodemon package for node auto reloads

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const exApp = express();
 const http = require("http").createServer(exApp);
 const io = require("socket.io")(http);
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 exApp.use(express.static("public"));
 
